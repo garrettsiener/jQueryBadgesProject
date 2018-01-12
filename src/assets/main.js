55a49fe1c35872246f5daed91b0705de4fd947b3
Garrett Siener <garrett.siener@la.gov>
2018-01-12 08:13:04 -0600

    initial commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/4141162.json',
+    dataType: 'jsonp',
+    success: function(response){
+      $.each(response, function(){
+        $('#badges').append("<div></div>").addClass('course');
+        $('.course').append("<h3>"+ this.data('title') +"</h3>");
+        $('.course').append("<img src="+ this.data('badge') +"></img>");
+        $('.course').append("<a href=\""+ this.data('url') + "target=\"_blank\""
+                            +">See Course</a>").addClass(btn);
+      });
+    }
+  });
+
 
 });
